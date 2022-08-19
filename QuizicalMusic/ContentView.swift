912095c12e551852cd5e2374e7016ff4f78769ac
James D Vieyra <james@vieyra.co.za>
2022-08-19 14:21:57 +0200

    Initial Commit

---
+++ QuizicalMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuizicalMusic
+//
+//  Created by IACD-011 on 2022/08/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuizicalMusic/QuizicalMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizicalMusicApp.swift
+//  QuizicalMusic
+//
+//  Created by IACD-011 on 2022/08/19.
+//
+
+import SwiftUI
+
+@main
+struct QuizicalMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
